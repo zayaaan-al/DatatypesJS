5471206591edbd5611ac764a9304cdd66bcaeb2d
AL ZAYAN P <alzayanpoothakkal1156@gmail.com>
2025-08-07 10:11:10 +0530

    initial commit

---
+++ operators.js
@@ -0,0 +1,19 @@
+
+// arithmetic operators//
+
+let a =20;
+let b =8;
+console.log(a+b);
+console.log(a%b);
+console.log(a/b);
+console.log(a--);
+console.log(b++);
+
+// assignment operators //
+let x= 30;
+console.log(x+=10);
+x%=4;
+x*=2;
+
+// Comparison Operators //
+
---
+++ script.js
@@ -0,0 +1,61 @@
+//non primitive data types (objects)//
+// let student ={
+//     firstname :'Zayan',
+//     course: 'Mern',
+//     Age : 22
+
+
+// }
+// student.college='malabar';
+// student.course='BCA';
+// console.log(student)
+
+
+// a=2
+// if (a>0){
+//     console.log("Positive");
+// }
+// else if(a<0){
+//     console.log("Negative");
+// }
+// else {
+//     console.log("Zero");
+// }
+
+
+// for (var i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+for (var i=1;i<=6;i++ ){
+    console.log("*".repeat(i) )
+}
+
+for(var i=6; i>=1;i--){
+    console.log("*".repeat(i))
+}
+
+// amount=1000
+// if (amount>1000){
+//     discount = amount * 0.10;
+//     finalamount= amount - discount;
+//     console.log("10% Discount Earned");
+//     console.log("Amount :"+ finalamount);
+// }
+//  else if (amount>500){
+//      discount = amount * 0.05;
+//      finalamount= amount - discount;
+//     console.log("5% discount earned");
+//     console.log("Amount :"+ finalamount)
+//  } 
+//  else {
+//     console.log("no discount");
+    
+//  }
+1 
+2*2
+3*3*3
+4*4*4*4
+3*3*3
+2*2
+1 
